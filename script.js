34b4892a8e2f848290ab9313f8078796d0db1eac
Cann2410 <24103346@st.phenikaa-uni.edu.vn>
2026-09-11 14:32:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function upDate(previewPic) {
 
+    console.log("upDate function triggered");
+
+    console.log("Alt:", previewPic.alt);
+    console.log("Source:", previewPic.src);
+
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+
+    document.getElementById("image").innerHTML =
+        previewPic.alt;
+}
+
+
+function unDo() {
+
+    document.getElementById("image").style.backgroundImage =
+        "url('')";
+
+    document.getElementById("image").innerHTML =
+        "Hover over an image below to display here.";
+}
